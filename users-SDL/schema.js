8fe8bcfdab341096693f1ef185b5eeca86ceec23
Brian Mullan <bmullan91@gmail.com>
2017-06-28 14:40:41 +0100

    update users-SDL - make the person do more

--- users-SDL/schema.js
+++ users-SDL/schema.js
@@ -7,51 +7,36 @@ const db = require('./db');
 // this supports a more flexible way to define resolvers
 const { makeExecutableSchema } = require('graphql-tools');
 
+
+// TODO: Type Definitions
+// ----
+// 1. Define the 'Query' type
+//  - find a user by id
+//  - get a list of users, and limit it
+// 2. Define the 'User' type
+//  - id, firstName, lastName, email, profilePic -> GravatarPic
+// 3. Define the 'GravatarPic' type
+//  - url field
+// 4. Define the 'Mutation' type
+//  - user_create, firstName, lastName, email -> User
 const typeDefs = `
-  type User {
-    id: ID!
-    email: String
-    firstName: String
-    lastName: String
-    # TODO: 1
-    # define a new field: profilePic
-    # it expects a required argument 'size' of type Int
-    # its return type is GravatarPic
-  }
-
-  # TODO: 2
-  # define a new type 'GravatarPic'
-  # it has a 'url' field of type String
-
-  type Query {
-    user(id: String!): User
-    users(limit: Int): [User]
-  }
-
-  # TODO: 4
-  # Define a new type 'Mutation'
-  # add a field 'user_create'
-  # it should take the following arguments: email, firstName & lastName
-  # it's return type is User
-  # next: update the resolvers below to include Mutation.user_create
+  # TODO!
 `;
 
+
+// TODO: Resolvers
+// 1. Query.user(id) -> User
+// 2. Query.users(limit) -> [User]
+// 2. User.profilePic...
+// This is a derived field, it's not actually on the user model
+// but like any field - it's just a function
+// we can add abriatary code in it's resolve function
+// gravatar.com images are identified by the users email, hashed by md5
+// https://www.gravatar.com/avatar/${md5(user.email)}?s=${size}
+// remember its an object we're expecting with a url prop
+// 4. Mutation.user_create(fn, ln, email) -> User
 const resolvers = {
-  Query: {
-    user: (root, { id }) => db.Users.read({ id }),
-    users: (root, { limit = 20 }) => db.Users.read({ limit }),
-  },
-  User: {
-    // TODO: 3
-    // ----
-    // Add the profilePic resolver here
-    // This is a derived field, it's not actually on the user model
-    // but like any field - it's just a function
-    // we can add abriatary code in it's resolve function
-    // gravatar.com images are identified by the users email, hashed by md5
-    // https://www.gravatar.com/avatar/${md5(user.email)}?s=${size}
-    // remember its an object we're expecting with a url prop
-  }
+  // TODO!
 };
 
 module.exports = makeExecutableSchema({
